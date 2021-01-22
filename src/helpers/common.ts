49568acf48fb1506c0e95ef4f2481583f39d8640
Rodrigo R. Almeidam <rodrigo.roberto.almeida@gmail.com>
2021-01-22 03:14:37 -0300

    trying to fix husky

--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -4,7 +4,11 @@
  * @param defaultValue if tag value is undefined will return this default value
  * @param disablePrefix disable 'NEXT_PUBLIC_' prefix
  */
-export const getEnv = (tagName: string, defaultValue?: string, disablePrefix?: boolean): string | undefined => {
+export const getEnv = (
+  tagName: string,
+  defaultValue?: string,
+  disablePrefix?: boolean
+): string | undefined => {
   const prefix = 'NEXT_PUBLIC_';
   if (process.env.public) {
     const env = process.env.public[!disablePrefix ? prefix + tagName : tagName];
